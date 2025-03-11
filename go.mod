a993b18f309761c1ff2ceae791dabe688b295953
John Verity <john@flowrolled.com>
2025-03-11 19:15:26 -0400

    zips output

--- go.mod
+++ go.mod
@@ -1,10 +1,8 @@
 module github.com/realjv3/imgResizer
 
-go 1.24
-
-require github.com/go-chi/chi/v5 v5.2.1
+go 1.24.0
 
 require (
-	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
-	gopkg.in/gographics/imagick.v2 v2.7.0 // indirect
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 )
--- rest/img.go
+++ rest/img.go
@@ -1,8 +1,10 @@
 package rest
 
 import (
+	"archive/zip"
 	"fmt"
 	"image/jpeg"
+	"io"
 	"net/http"
 	"os"
 	"strconv"
@@ -31,6 +33,17 @@ func Resize(w http.ResponseWriter, r *http.Request) {
 		return
 
 	}
+
+	output, err := os.Create("output.zip")
+	if err != nil {
+		http.Error(w, "error creating output.zip", http.StatusInternalServerError)
+		return
+	}
+	defer output.Close()
+
+	zipWriter := zip.NewWriter(output)
+	defer zipWriter.Close()
+
 	for _, files := range r.MultipartForm.File {
 		for _, file := range files {
 			// imagick load image
@@ -39,7 +52,7 @@ func Resize(w http.ResponseWriter, r *http.Request) {
 
 			img, err := jpeg.Decode(f)
 			if err != nil {
-				http.Error(w, fmt.Sprintf("error reading into imagick: %v"), http.StatusInternalServerError)
+				http.Error(w, fmt.Sprintf("error reading into imagick: %w", err), http.StatusInternalServerError)
 				return
 			}
 
@@ -49,18 +62,76 @@ func Resize(w http.ResponseWriter, r *http.Request) {
 			resized := resize.Resize(uint(rows), uint(cols), img, resize.Lanczos2)
 			out, err := os.Create(file.Filename)
 			if err != nil {
-				http.Error(w, fmt.Sprintf("error creating file: %v"), http.StatusInternalServerError)
+				http.Error(w, fmt.Sprintf("error creating file: %w", err), http.StatusInternalServerError)
 				return
 			}
+
 			err = jpeg.Encode(out, resized, nil)
 			if err != nil {
-				http.Error(w, fmt.Sprintf("error encoding jpeg: %v"), http.StatusInternalServerError)
+				http.Error(w, fmt.Sprintf("error encoding jpeg: %w", err), http.StatusInternalServerError)
+				return
+			}
+
+			// out needs to be closed and reopened to reset the file pointer back to the beginning of the file
+			err = out.Close()
+			if err != nil {
+				http.Error(w, fmt.Sprintf("error closing temporary jpeg: %w", err), http.StatusInternalServerError)
+				return
+			}
+
+			out, err = os.Open(file.Filename)
+			if err != nil {
+				http.Error(w, fmt.Sprintf("error opening temporary jpeg: %w", err), http.StatusInternalServerError)
+				return
+			}
+
+			err = zipFile(file.Filename, out, zipWriter)
+			if err != nil {
+				http.Error(w, fmt.Sprintf("error zipping jpeg: %w", err), http.StatusInternalServerError)
+				return
+			}
+
+			err = os.Remove(file.Filename)
+			if err != nil {
+				http.Error(w, fmt.Sprintf("error deleting temporary jpeg: %w", err), http.StatusInternalServerError)
 				return
 			}
 		}
 	}
 
-	w.Write([]byte("Images resized ok."))
+	w.Header().Set("Content-Type", "application/zip")
+	w.Header().Set("Content-Disposition", "attachment; filename=\"output.zip\"")
+	io.Copy(w, output)
 
 	return
+}
+
+func zipFile(filename string, file io.Reader, zipWriter *zip.Writer) error {
+	fileInfo, err := os.Stat(filename)
+	if err != nil {
+		return err
+	}
+
+	header, err := zip.FileInfoHeader(fileInfo)
+	if err != nil {
+		return err
+	}
+	header.Name = filename
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+
+	n, err := io.Copy(writer, file)
+	if err != nil {
+		return err
+	}
+
+	if n != fileInfo.Size() {
+		return fmt.Errorf("wrote %d bytes instead of expected %d for %s", n, fileInfo.Size(), filename)
+	}
+
+	return nil
 }
